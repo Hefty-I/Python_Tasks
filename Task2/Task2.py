d6e34aeff46c2c8a8a4e75379b7f2e7d367bf846
Hassaan Siddiqui <hassaansiddiqui818@gmail.com>
2025-07-09 17:07:03 +0500

    Completed the task

--- Task2/Task2.py
+++ Task2/Task2.py
@@ -1,13 +1,18 @@
 City = {
 }
 
-for i in range(3):
-        x = input("Enter a city: ")
-        City[f"city{i}"] = x
+# for i in range(3):
+#         x = input("Enter a city: ")
+#         City[f"city{i}"] = x
 
 with open("city.txt", 'w') as f:
-    for key,values in City.items():
-        f.write(f"{key} "+ x +"\n")
+    
+    for i in range(3): # run loop 3 times
+        x = input("Enter a city: ")
+        City[f"city{i}"] = x # add the input city to dictionary
+
+    for key, values in City.items(): 
+        f.write(f"{key} "+ values +"\n") # write to the file key of dictionary along with the value
 
 with open("city.txt",'r') as f:
         print(f.read())
